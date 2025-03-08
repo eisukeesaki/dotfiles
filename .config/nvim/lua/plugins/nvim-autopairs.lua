d1006fb9540f402c087dfce2ace449ca1486ae1a
Eisuke Esaki <eisukeesaki@gmail.com>
2025-03-07 23:24:20 -0800

    autopairs backtick

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,10 +1,12 @@
-require('nvim-autopairs').remove_rule('`')
-
 return {
   {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
+    -- config = true, -- default
+    config = function()
+      require('nvim-autopairs').setup({})
+      require('nvim-autopairs').remove_rule('`')
+    end,
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
   }
